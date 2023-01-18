48e49094399f1bbe9062331ec4538cd9c4d34de4
MryImani <maryam_imani22@yahoo.com>
2023-01-18 17:03:30 +0330

    Improve the project including the search engine, API integration, unit conversion, wind speed, weather description, and weather icon are mandatory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,71 @@
-let theme = {};
+/*
+sunny = url('https://images.pexels.com/photos/1303216/pexels-photo-1303216.jpeg?auto=compress&cs=tinysrgb&w=1600');
+cloudy = url("https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?auto=compress&cs=tinysrgb&w=1600");
+https://images.pexels.com/photos/14944496/pexels-photo-14944496.jpeg?auto=compress&cs=tinysrgb&w=1600;
+
+partly-cloudy = url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600');
+snowy = url('https://images.pexels.com/photos/6618008/pexels-photo-6618008.jpeg?auto=compress&cs=tinysrgb&w=1600');
+https://images.pexels.com/photos/3334473/pexels-photo-3334473.jpeg?auto=compress&cs=tinysrgb&w=1600
+https://images.pexels.com/photos/6617796/pexels-photo-6617796.jpeg?auto=compress&cs=tinysrgb&w=1600
+https://images.pexels.com/photos/760110/pexels-photo-760110.jpeg?auto=compress&cs=tinysrgb&w=1600
+https://images.pexels.com/photos/2423351/pexels-photo-2423351.jpeg?auto=compress&cs=tinysrgb&w=1600
+
+rainy = url('https://images.pexels.com/photos/9065305/pexels-photo-9065305.jpeg?auto=compress&cs=tinysrgb&w=1600');
+*/
+let theme = {
+  Clouds: {
+    backgroundUrl:
+      "url('https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+
+    iconSrc: "images/cloudy.png",
+  },
+  partlyCloudy: {
+    backgroundUrl:
+      "url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+    iconSrc: "images/partly-cloudy.png",
+  },
+  Clear: {
+    backgroundUrl:
+      "url('https://images.pexels.com/photos/1303216/pexels-photo-1303216.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+    iconSrc: "images/sunny.png",
+  },
+  Atmosphere: {
+    backgroundUrl: "",
+    iconSrc: "http://openweathermap.org/img/wn/50d@2x.png",
+  },
+  Snow: {
+    backgroundUrl:
+      "url('https://images.pexels.com/photos/2423351/pexels-photo-2423351.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+    iconSrc: "images/snowy.png",
+  },
+  Rain: {
+    backgroundUrl:
+      "url('https://images.pexels.com/photos/9065305/pexels-photo-9065305.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+    iconSrc: "images/rainy.png",
+  },
+  Thunderstorm: {
+    backgroundUrl: "",
+    iconSrc: "http://openweathermap.org/img/wn/11d@2x.png",
+  },
+};
+
+function getTheme(id) {
+  if (id < 300) {
+    return theme.Thunderstorm;
+  } else if (id >= 300 && id < 600) {
+    return theme.Rain;
+  } else if (id >= 600 && id < 700) {
+    return theme.Snow;
+  } else if (id >= 700 && id < 800) {
+    return theme.Atmosphere;
+  } else if ((id = 800)) {
+    return theme.Clear;
+  } else if ((id = 801)) {
+    return theme.partlyCloudy;
+  } else if (id >= 802) {
+    return theme.Clouds;
+  }
+}
 //get current Date and Time
 function getDateAndTime(date) {
   let days = [
@@ -66,10 +133,31 @@ function showWeatherInCurrentPosition(position) {
       `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
     )
     .then((response) => {
-      document.querySelector(".currentStatusTemperature .celsius").innerHTML =
-        Math.floor(response.data.main.temp);
+      document.querySelector(
+        ".currentStatusTemperature .temperature"
+      ).innerHTML = Math.floor(response.data.main.temp);
       document.getElementById("cityName").innerHTML = response.data.name;
       document.getElementById("country").innerHTML = response.data.sys.country;
+      document.querySelector(".weatherDescription").innerHTML =
+        response.data.weather[0].description;
+      document.querySelectorAll(".humidity").forEach((item) => {
+        item.innerHTML = `${response.data.main.humidity}%`;
+      });
+      document.querySelectorAll(".wind").forEach((item) => {
+        item.innerHTML = `${response.data.wind.speed}km/h`;
+      });
+      //set theme
+      document.querySelector(".currentStatusLogo img").src = getTheme(
+        response.data.weather[0].id
+      ).iconSrc;
+      document.querySelector(
+        ".theme"
+      ).style.backgroundImage = `linear-gradient(black, black),${
+        getTheme(response.data.weather[0].id).backgroundUrl
+      }`;
+      document.querySelector(".container").style.backgroundImage = getTheme(
+        response.data.weather[0].id
+      ).backgroundUrl;
     });
 }
 
@@ -93,11 +181,31 @@ https: document
         .get(`${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`)
         .then((response) => {
           document.querySelector(
-            ".currentStatusTemperature .celsius"
+            ".currentStatusTemperature .temperature"
           ).innerHTML = Math.floor(response.data.main.temp);
           document.getElementById("cityName").innerHTML = response.data.name;
           document.getElementById("country").innerHTML =
             response.data.sys.country;
+          document.querySelector(".weatherDescription").innerHTML =
+            response.data.weather[0].description;
+          document.querySelectorAll(".humidity").forEach((item) => {
+            item.innerHTML = `${response.data.main.humidity}%`;
+          });
+          document.querySelectorAll(".wind").forEach((item) => {
+            item.innerHTML = `${response.data.wind.speed}km/h`;
+          });
+          //set theme
+          document.querySelector(".currentStatusLogo img").src = getTheme(
+            response.data.weather[0].id
+          ).iconSrc;
+          document.querySelector(
+            ".theme"
+          ).style.backgroundImage = `linear-gradient(black, black),${
+            getTheme(response.data.weather[0].id).backgroundUrl
+          }`;
+          document.querySelector(".container").style.backgroundImage = getTheme(
+            response.data.weather[0].id
+          ).backgroundUrl;
         });
     } else {
       alert("City not found,Please try again!");
@@ -118,21 +226,22 @@ function fToC(fahrenheit) {
   var result = `${fToCel}`;
   return result;
 }
-
-//change temp from celsius to fahrenheit
-let tempList = document.querySelectorAll(".convertTemp");
-tempList.forEach((elm) => {
-  elm.addEventListener("click", (e) => {
-    if (e.target.innerHTML === "°C") {
-      e.target.previousElementSibling.innerHTML = cToF(
-        e.target.previousElementSibling.innerHTML
-      );
-      e.target.innerHTML = "°F";
-    } else if (e.target.innerHTML === "°F") {
-      e.target.previousElementSibling.innerHTML = fToC(
-        e.target.previousElementSibling.innerHTML
-      );
-      e.target.innerHTML = "°C";
-    }
+//convert to fahrenheit
+let ctfBtn = document.getElementById("btnradio2");
+ctfBtn.addEventListener("click", () => {
+  let tempList = document.querySelectorAll(".temperature");
+  tempList.forEach((elm) => {
+    elm.innerHTML = cToF(elm.innerHTML);
   });
+  document.querySelector(".temperatureUnit").innerHTML = "°F";
+});
+
+//convert to celsius
+let ftcBtn = document.getElementById("btnradio1");
+ftcBtn.addEventListener("click", () => {
+  let tempList = document.querySelectorAll(".temperature");
+  tempList.forEach((elm) => {
+    elm.innerHTML = fToC(elm.innerHTML);
+  });
+  document.querySelector(".temperatureUnit").innerHTML = "°C";
 });
