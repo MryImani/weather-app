b25c2b8d36e4595ac7268519f45a552b1caf9d2e
MryImani <maryam_imani22@yahoo.com>
2023-01-21 18:53:12 +0330

    change api , create preloader

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
-/*
-sunny = url('https://images.pexels.com/photos/1303216/pexels-photo-1303216.jpeg?auto=compress&cs=tinysrgb&w=1600');
-cloudy = url("https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?auto=compress&cs=tinysrgb&w=1600");
-https://images.pexels.com/photos/14944496/pexels-photo-14944496.jpeg?auto=compress&cs=tinysrgb&w=1600;
-
-partly-cloudy = url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600');
-snowy = url('https://images.pexels.com/photos/6618008/pexels-photo-6618008.jpeg?auto=compress&cs=tinysrgb&w=1600');
-https://images.pexels.com/photos/3334473/pexels-photo-3334473.jpeg?auto=compress&cs=tinysrgb&w=1600
-https://images.pexels.com/photos/6617796/pexels-photo-6617796.jpeg?auto=compress&cs=tinysrgb&w=1600
-https://images.pexels.com/photos/760110/pexels-photo-760110.jpeg?auto=compress&cs=tinysrgb&w=1600
-https://images.pexels.com/photos/2423351/pexels-photo-2423351.jpeg?auto=compress&cs=tinysrgb&w=1600
-
-rainy = url('https://images.pexels.com/photos/9065305/pexels-photo-9065305.jpeg?auto=compress&cs=tinysrgb&w=1600');
-*/
 let theme = {
   Clouds: {
     backgroundUrl:
-      "url('https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      "url('https://images.pexels.com/photos/14944496/pexels-photo-14944496.jpeg?auto=compress&cs=tinysrgb&w=1600')",
 
     iconSrc: "images/cloudy.png",
   },
   partlyCloudy: {
     backgroundUrl:
-      "url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      "url('https://images.pexels.com/photos/1107717/pexels-photo-1107717.jpeg?auto=compress&cs=tinysrgb&w=1600')",
     iconSrc: "images/partly-cloudy.png",
   },
   Clear: {
     backgroundUrl:
-      "url('https://images.pexels.com/photos/1303216/pexels-photo-1303216.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      "url('https://images.pexels.com/photos/33622/fashion-model-beach-hat.jpg?auto=compress&cs=tinysrgb&w=1600')",
     iconSrc: "images/sunny.png",
   },
   Atmosphere: {
-    backgroundUrl: "",
-    iconSrc: "http://openweathermap.org/img/wn/50d@2x.png",
+    backgroundUrl:
+      "https://images.pexels.com/photos/355810/pexels-photo-355810.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    iconSrc:
+      "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-day.png",
   },
   Snow: {
     backgroundUrl:
-      "url('https://images.pexels.com/photos/2423351/pexels-photo-2423351.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      "url('https://images.pexels.com/photos/760110/pexels-photo-760110.jpeg?auto=compress&cs=tinysrgb&w=1600')",
     iconSrc: "images/snowy.png",
   },
   Rain: {
@@ -44,25 +32,42 @@ let theme = {
     iconSrc: "images/rainy.png",
   },
   Thunderstorm: {
-    backgroundUrl: "",
-    iconSrc: "http://openweathermap.org/img/wn/11d@2x.png",
+    backgroundUrl:
+      "https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    iconSrc:
+      "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/thunderstorm-day.png",
   },
 };
 
-function getTheme(id) {
-  if (id < 300) {
+function getTheme(weatherDescription) {
+  if (weatherDescription.includes("thunderstorm")) {
     return theme.Thunderstorm;
-  } else if (id >= 300 && id < 600) {
+  } else if (weatherDescription.includes("rain")) {
+    document.querySelectorAll("ul p").forEach((elm) => {
+      elm.style.color = "#eae9ec";
+    });
     return theme.Rain;
-  } else if (id >= 600 && id < 700) {
+  } else if (weatherDescription.includes("snow")) {
     return theme.Snow;
-  } else if (id >= 700 && id < 800) {
+  } else if (weatherDescription.includes("mist")) {
     return theme.Atmosphere;
-  } else if (id == 800) {
+  } else if (weatherDescription.includes("haze")) {
+    return theme.Atmosphere;
+  } else if (weatherDescription.includes("clear sky")) {
+    document.querySelectorAll("ul p").forEach((elm) => {
+      elm.style.color = "#eae9ec";
+    });
     return theme.Clear;
-  } else if (id == 801) {
+  } else if (weatherDescription.includes("few clouds")) {
+    document.querySelectorAll("ul p").forEach((elm) => {
+      elm.style.color = "#eae9ec";
+    });
     return theme.partlyCloudy;
-  } else if (id >= 802) {
+  } else if (weatherDescription.includes("clouds")) {
+    document.querySelectorAll("ul p").forEach((elm) => {
+      elm.style.color = "#99979d";
+    });
+
     return theme.Clouds;
   }
 }
@@ -164,17 +169,22 @@ function getDateAndTime(date) {
   }${strTime}, ${date.getDate()} ${
     months[date.getMonth()]
   } ${date.getFullYear()}`;
-  utc = date.getTime() + date.getTimezoneOffset() * 60000;
-  console.log(new Date(utc));
 }
-
 getDateAndTime(new Date());
+function getDateBasedOnTimestamp(unixTimestamp) {
+  var date_unixTimestamp = new Date(unixTimestamp * 1000);
+  getDateAndTime(date_unixTimestamp);
+}
 
 //get user location
 let currentLocation = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 document.getElementById("cityName").innerHTML = currentLocation.split("/")[1];
 document.getElementById("country").innerHTML = currentLocation.split("/")[0];
+window.addEventListener("load", () => {
+  document.querySelector(".content").style.display = "none";
+  weatherInCity(currentLocation.split("/")[1]);
+});
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showWeatherInCurrentPosition);
@@ -184,40 +194,47 @@ function getLocation() {
 }
 
 function showWeatherInCurrentPosition(position) {
-  let apiKey = "30a656996750d42d88b01c05992431b9";
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "5fd3056eb5affa9b30928e4047a72otd";
+  let apiUrl = "https://api.shecodes.io/weather/v1/forecast";
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   axios
     .get(
-      `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      `${apiUrl}?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`
     )
     .then((response) => {
-      document.querySelector(
-        ".currentStatusTemperature .temperature"
-      ).innerHTML = Math.floor(response.data.main.temp);
-      document.getElementById("cityName").innerHTML = response.data.name;
-      document.getElementById("country").innerHTML = response.data.sys.country;
-      document.querySelector(".weatherDescription").innerHTML =
-        response.data.weather[0].description;
-      document.querySelectorAll(".humidity").forEach((item) => {
-        item.innerHTML = `${response.data.main.humidity}%`;
-      });
-      document.querySelectorAll(".wind").forEach((item) => {
-        item.innerHTML = `${response.data.wind.speed}km/h`;
-      });
-      //set theme
-      document.querySelector(".currentStatusLogo img").src = getTheme(
-        response.data.weather[0].id
-      ).iconSrc;
-      document.querySelector(
-        ".theme"
-      ).style.backgroundImage = `linear-gradient(black, black),${
-        getTheme(response.data.weather[0].id).backgroundUrl
-      }`;
-      document.querySelector(".container").style.backgroundImage = getTheme(
-        response.data.weather[0].id
-      ).backgroundUrl;
+      if (response.status === 200) {
+        setTimeout(function () {
+          getDateBasedOnTimestamp(response.data.time);
+          document.querySelector(
+            ".currentStatusTemperature .temperature"
+          ).innerHTML = Math.floor(response.data.temperature.current);
+          document.getElementById("cityName").innerHTML = response.data.city;
+          document.getElementById("country").innerHTML = response.data.country;
+          document.querySelector(".weatherDescription").innerHTML =
+            response.data.condition.description;
+          document.querySelectorAll(".humidity").forEach((item) => {
+            item.innerHTML = `${response.data.temperature.humidity}%`;
+          });
+          document.querySelectorAll(".wind").forEach((item) => {
+            item.innerHTML = `${response.data.wind.speed}km/h`;
+          });
+          //set theme
+          document.querySelector(".currentStatusLogo img").src = getTheme(
+            response.data.condition.description.toLowerCase()
+          ).iconSrc;
+          document.querySelector(
+            ".theme"
+          ).style.backgroundImage = `linear-gradient(black, black),${
+            getTheme(response.data.condition.description.toLowerCase())
+              .backgroundUrl
+          }`;
+          document.querySelector(".container").style.backgroundImage = getTheme(
+            response.data.condition.description.toLowerCase()
+          ).backgroundUrl;
+          dailyForecast(latitude, longitude, apiKey);
+        }, 3000);
+      }
     });
 }
 
@@ -228,39 +245,55 @@ document
 //get weather status by search city name
 
 function weatherInCity(cityName) {
-  let apiKey = "30a656996750d42d88b01c05992431b9";
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+  document.querySelector(".content").style.display = "none";
+  document.querySelector(".preLoader").style.display = "flex";
+  let apiKey = "5fd3056eb5affa9b30928e4047a72otd";
+  let apiUrl = "https://api.shecodes.io/weather/v1/current";
   if (cityName) {
     axios
-      .get(`${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`)
+      .get(`${apiUrl}?query=${cityName}&key=${apiKey}&units=metric`)
       .then((response) => {
-        document.querySelector(
-          ".currentStatusTemperature .temperature"
-        ).innerHTML = Math.floor(response.data.main.temp);
-        document.getElementById("cityName").innerHTML = response.data.name;
-        document.getElementById("country").innerHTML =
-          response.data.sys.country;
-        document.querySelector(".weatherDescription").innerHTML =
-          response.data.weather[0].description;
-        document.querySelectorAll(".humidity").forEach((item) => {
-          item.innerHTML = `${response.data.main.humidity}%`;
-        });
-        document.querySelectorAll(".wind").forEach((item) => {
-          item.innerHTML = `${response.data.wind.speed}km/h`;
-        });
-        //set theme
-        document.querySelector(".currentStatusLogo img").src = getTheme(
-          response.data.weather[0].id
-        ).iconSrc;
-        document.querySelector(
-          ".theme"
-        ).style.backgroundImage = `linear-gradient(black, black),${
-          getTheme(response.data.weather[0].id).backgroundUrl
-        }`;
-        document.querySelector(".container").style.backgroundImage = getTheme(
-          response.data.weather[0].id
-        ).backgroundUrl;
-        dailyForecast(response.data.coord.lat, response.data.coord.lon, apiKey);
+        if (response.status === 200) {
+          setTimeout(function () {
+            getDateBasedOnTimestamp(response.data.time);
+            document.querySelector(
+              ".currentStatusTemperature .temperature"
+            ).innerHTML = Math.floor(response.data.temperature.current);
+            document.getElementById("cityName").innerHTML = response.data.city;
+            document.getElementById("country").innerHTML =
+              response.data.country;
+            document.querySelector(".weatherDescription").innerHTML =
+              response.data.condition.description;
+            document.querySelectorAll(".humidity").forEach((item) => {
+              item.innerHTML = `${response.data.temperature.humidity}%`;
+            });
+            document.querySelectorAll(".wind").forEach((item) => {
+              item.innerHTML = `${response.data.wind.speed}km/h`;
+            });
+            //set theme
+            document.querySelector(".currentStatusLogo img").src = getTheme(
+              response.data.condition.description.toLowerCase()
+            ).iconSrc;
+
+            document.querySelector(
+              ".theme"
+            ).style.backgroundImage = `linear-gradient(black, black),${
+              getTheme(response.data.condition.description.toLowerCase())
+                .backgroundUrl
+            }`;
+            document.querySelector(".container").style.backgroundImage =
+              getTheme(
+                response.data.condition.description.toLowerCase()
+              ).backgroundUrl;
+            dailyForecast(
+              response.data.coordinates.latitude,
+              response.data.coordinates.longitude,
+              apiKey
+            );
+            document.querySelector(".content").style.display = "flex";
+            document.querySelector(".preLoader").style.display = "none";
+          }, 3000);
+        }
       });
   } else {
     alert("City not found,Please try again!");
@@ -274,7 +307,6 @@ https: document
     event.preventDefault();
     let cityName = document.getElementById("searchCityInp").value;
     cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
-    let country = "";
     weatherInCity(cityName);
   });
 
@@ -282,77 +314,21 @@ https: document
 function dailyForecast(lat, lon, apiKey) {
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&cnt=6&units=metric`
+      `https://api.shecodes.io/weather/v1/forecast?lat=${lat}&lon=${lon}&key=${apiKey}&units=metric`
     )
     .then((response) => {
-      //set icon
-      document.querySelector(".nextDay1 img").src = getTheme(
-        response.data.list[0].weather[0].id
-      ).iconSrc;
-      document.querySelector(".nextDay2 img").src = getTheme(
-        response.data.list[1].weather[0].id
-      ).iconSrc;
-      document.querySelector(".nextDay3 img").src = getTheme(
-        response.data.list[2].weather[0].id
-      ).iconSrc;
-      document.querySelector(".nextDay4 img").src = getTheme(
-        response.data.list[3].weather[0].id
-      ).iconSrc;
-      document.querySelector(".nextDay5 img").src = getTheme(
-        response.data.list[4].weather[0].id
-      ).iconSrc;
-      document.querySelector(".nextDay6 img").src = getTheme(
-        response.data.list[5].weather[0].id
-      ).iconSrc;
+      //set daily icon & temperature
+      for (let i = 0; i < 6; i++) {
+        document.querySelector(`.nextDay${i + 1} img`).src =
+          response.data.daily[i].condition.icon_url;
+        document.querySelector(`.nextDay${i + 1} .max`).innerHTML = Math.floor(
+          response.data.daily[i].temperature.maximum
+        );
 
-      //set temperature
-      document.querySelector(".nextDay1 .max").innerHTML = Math.floor(
-        response.data.list[0].main.temp_max
-      );
-
-      document.querySelector(".nextDay1 .min").innerHTML = Math.floor(
-        response.data.list[0].main.temp_min
-      );
-
-      document.querySelector(".nextDay2 .max").innerHTML = Math.floor(
-        response.data.list[1].main.temp_max
-      );
-
-      document.querySelector(".nextDay2 .min").innerHTML = Math.floor(
-        response.data.list[1].main.temp_min
-      );
-
-      document.querySelector(".nextDay3 .max").innerHTML = Math.floor(
-        response.data.list[2].main.temp_max
-      );
-
-      document.querySelector(".nextDay3 .min").innerHTML = Math.floor(
-        response.data.list[2].main.temp_min
-      );
-
-      document.querySelector(".nextDay4 .max").innerHTML = Math.floor(
-        response.data.list[3].main.temp_max
-      );
-
-      document.querySelector(".nextDay4 .min").innerHTML = Math.floor(
-        response.data.list[3].main.temp_min
-      );
-
-      document.querySelector(".nextDay5 .max").innerHTML = Math.floor(
-        response.data.list[4].main.temp_max
-      );
-
-      document.querySelector(".nextDay5 .min").innerHTML = Math.floor(
-        response.data.list[4].main.temp_min
-      );
-
-      document.querySelector(".nextDay6 .max").innerHTML = Math.floor(
-        response.data.list[5].main.temp_max
-      );
-
-      document.querySelector(".nextDay6 .min").innerHTML = Math.floor(
-        response.data.list[5].main.temp_min
-      );
+        document.querySelector(`.nextDay${i + 1} .min`).innerHTML = Math.floor(
+          response.data.daily[i].temperature.minimum
+        );
+      }
     });
 }
 //convert temperature
@@ -388,5 +364,3 @@ ftcBtn.addEventListener("click", () => {
   });
   document.querySelector(".temperatureUnit").innerHTML = "°C";
 });
-
-weatherInCity("Mashhad");
